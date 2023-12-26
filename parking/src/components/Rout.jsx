774bbec7c821dafb9e5b83cb3428800d872caebf
009661@#m <tari.almutairi1995@gmai.com>
2023-12-26 10:55:24 +0300

    amendment

--- parking/src/components/Rout.jsx
+++ parking/src/components/Rout.jsx
@@ -3,15 +3,27 @@ import {Routes,Route} from 'react-router-dom'
 import LnadingPage from './LnadingPage'
 import TestEvreyThing from './TestEvreyThing'
 import BookInfo from './BookInfo'
+import Signup from './Signup'
+import Singnin from './Singnin'
+import Support from './Support'
+import UserData from './UserData'
+import Test from './Test'
 
 export default function Rout() {
   return (
     <>
-    <Routes>
+    {/* <Routes>
         <Route path='/' element={<LnadingPage/>}></Route>
         <Route path='/bookinfo' element={<BookInfo/>} />
 
-    </Routes>  
+    </Routes>   */}
+
+    {/* <Singnin/> */}
+    <Signup/>
+    {/* <Support/> */}
+    {/* <UserData/> */}
+    {/* <Test/> */}
+
     </>
   )
 }
