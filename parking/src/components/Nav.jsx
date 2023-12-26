448d3de42a2bd2d3e5533462865c41a0ec0e1608
Arwa-Alsaadan <arwa.s.1997@hotmail.com>
2023-12-26 14:37:56 +0300

    modify leadingPage

--- parking/src/components/Nav.jsx
+++ parking/src/components/Nav.jsx
@@ -6,12 +6,12 @@ import logo from '../assets/Logo.png'
 export default function Nav() {
   return (
     <>
-    
-  <div dir='rtl'>
+   
+  <div dir='rtl' className='w-full'>
 
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <img src={logo} className='h-[10vh] mr-4'></img>
+    <img src={logo} className='h-[10vh]'></img>
     {/* <a class="navbar-brand" href="#">Navbar w/ text</a> */}
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
