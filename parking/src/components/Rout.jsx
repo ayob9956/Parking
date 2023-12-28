65a4257c483152be0a8a77e82fb77183a951b111
ayob9956 <ayobalayed@gmail.com>
2023-12-28 20:41:33 +0300

    add payment gateway and hundel some components

--- parking/src/components/Rout.jsx
+++ parking/src/components/Rout.jsx
@@ -13,19 +13,31 @@ import Support from './Support'
 import UserData from './UserData'
 import Test from './Test'
 import Nav from './Nav'
-
+import CheckoutForm from './CheckoutForm'
  import Ticket from './Ticket'
-
+ import {Elements} from '@stripe/react-stripe-js';
+ import {loadStripe} from '@stripe/stripe-js';
+ import Checkout from './Checkout'
+ 
+ // Make sure to call `loadStripe` outside of a component’s render to avoid
+ // recreating the `Stripe` object on every render.
+ const stripePromise = loadStripe('pk_test_51OS4liBEKzJHwgwjOgTTCfeco9oWGRtB8MNvhMDzGc9HxPc8eCsqUxIN26UCfJKbauiMzGukK8iolSM6OqN7qAsl00y9SkWZYz');
+ 
 
 
 
 export default function Rout() {
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: '{{CLIENT_SECRET}}',
+  };
   return (
     <>
     <Routes>
         <Route path='/' element={<LnadingPage/>}></Route>
         <Route path='/bookinfo' element={<BookInfo/>} />
         <Route path='/userdata' element={<UserData/>} />
+
 
         <Route path='/payment' element={<Payment/>}/>
         <Route path='/addCard' element={<AddCard/>}/>
@@ -36,6 +48,10 @@ export default function Rout() {
         <Route path='/support' element={<Support/>}/>
 
         <Route path='/ticket' element={<Ticket/>}/>
+       
+          <Route path='/checkout' element={<Checkout/>}/>
+
+
 
 
 
